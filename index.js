a2a0e267624dfa858fd3efbad9c0ba97b409f330
pftom <huan1043269994@gmail.com>
2018-09-10 14:42:53 +0800

    使用模板引擎

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Node"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => res.render("index", { info: "Node" }));
 
 app.listen(3000, () => {
   console.log("App is listening on port 3000 ...");
