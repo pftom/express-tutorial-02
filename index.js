9c69ab3a676592c3098a5273c3bd2560df25e49d
pftom <huan1043269994@gmail.com>
2018-09-14 18:52:29 +0800

    配置多页面的路由

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => res.render("explore", { info: "Node" }));
+app.get("/explore", (req, res) => res.render("explore", { info: "Node" }));
+app.get("/docs", (req, res) => res.render("docs", { info: "Node" }));
+app.get("/discussion", (req, res) =>
+  res.render("discussion", { info: "Node" })
+);
 
 app.listen(3000, () => {
   console.log("App is listening on port 3000 ...");
